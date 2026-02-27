ab7101c3a3108b0e0a50dfa68b8cbc63c948aba1
CanReader <canberk_nurcan@hotmail.com>
2026-02-27 11:30:00 +0300

    Add RenderUI method and UI state to MainScene

--- Game/include/MainScene.hpp
+++ Game/include/MainScene.hpp
@@ -5,6 +5,7 @@
 #include <Memory/RefPtr.h>
 #include <Events/MouseEvent.h>
 #include <Events/KeyboardEvent.h>
+#include <Debug/SystemMetrics.hpp>
 #include "World/ChunkManager.hpp"
 #include "World/Block.hpp"
 
@@ -22,6 +23,7 @@ private:
     void SetupMaterial();
     void SetupSkybox();
     void SetupLighting();
+    void RenderUI();
 
     void OnMousePressed(const Sleak::Events::Input::MouseButtonPressedEvent& e);
     void OnKeyPressed(const Sleak::Events::Input::KeyPressedEvent& e);
@@ -30,6 +32,12 @@ private:
     ChunkManager m_chunkManager;
     BlockType m_selectedBlock = BlockType::Grass;
     bool m_multithreadedLoading = true;
+
+    // UI state
+    bool m_showUI = true;
+    bool m_showColliders = false;
+    Sleak::SystemMetricsData m_cachedMetrics;
+    float m_metricTimer = 0.0f;
 };
 
 #endif
